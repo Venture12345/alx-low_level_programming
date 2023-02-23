5f821afc34f38807e752f873f4cb0c1edc1d67d6
Venture12345 <boniventuremassawe@gmail.com>
2023-02-23 00:21:21 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a function to check upper case alphas
+ * @c: an interger paramer
+ * Return: something but succes
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+
+        return (c >= 65 && c <= 90
+	return (c >= 'A' && c <= 'Z');
+}
